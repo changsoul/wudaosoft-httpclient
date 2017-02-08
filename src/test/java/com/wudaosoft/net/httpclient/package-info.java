a86d08d4cc859985f6fbf304dd4c0fe30647a0fc
changsoul <changsoul.wu@gmail.com>
2017-02-08 18:00:51 +0800

    add files

---
+++ src/test/java/com/wudaosoft/net/httpclient/package-info.java
@@ -0,0 +1,13 @@
+/* 
+ * Copyright(c)2010-2017 WUDAOSOFT.COM
+ * 
+ * Email:changsoul.wu@gmail.com
+ * 
+ * QQ:275100589
+ */
+
+/** 
+ * @author Changsoul Wu
+ * 
+ */
+package com.wudaosoft.net.httpclient;
